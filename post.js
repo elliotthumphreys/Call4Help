d5567072af9e79203594e311846367407235bc77
Elliott J. Humphreys <elliotthumphreys821@gmail.com>
2019-10-27 07:57:51 +0000

    somthing new?

--- post.js
+++ post.js
@@ -1,4 +1,10 @@
 const AWS = require('aws-sdk')
+
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_ACCESS_ID
+})
+
 const s3 = new AWS.S3()
 
 const covertObjectToBinary = obj => {
